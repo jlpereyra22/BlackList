d2022e15edcd17ea9796ddd8771f92583e5127be
Gwynbleid <pereyrajose848@gmail.com>
2025-09-07 18:31:01 -0300

    ultimas modificaciones al css del login, como margin, padding

--- veraz-interno/src/firebase.js
+++ veraz-interno/src/firebase.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
-console.log("[ENV KEYS]", Object.keys(import.meta.env).filter(k => k.includes("VITE_FIREBASE")));
-console.log("[ENV VALS]", {
-  API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
-  AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  APP_ID: import.meta.env.VITE_FIREBASE_APP_ID,
-  MEASUREMENT_ID: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-});
+
 
 
 const firebaseConfig = {
