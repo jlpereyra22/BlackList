6efddfe739ab64a3c80855baa36e065bc27c934e
Gwynbleid <pereyrajose848@gmail.com>
2025-09-14 18:35:35 -0300

    agregue un marca registrada en footer

--- veraz-interno/src/components/Pie.jsx
+++ veraz-interno/src/components/Pie.jsx
@@ -2,8 +2,8 @@ import "../style/Pie.css";
 
 export default function Pie() {
   return (
-    <footer>
-      <small>Uso interno. Datos con fines de prevención de riesgo.</small>
-    </footer>
+   <footer>
+  <p>Veraz Interno &reg; 2025</p>
+</footer>
   );
 }
